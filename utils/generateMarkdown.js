5f7f8024ee375b740e7a0f38a9c7a12dc8551bc1
hburnton <hburnton@gmail.com>
2022-09-28 11:42:45 -0700

    add readme structure and generation

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,36 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
+
+### Table of contents
+- [Description](#description)
+- [Installation Instructions](#installation-instructions)
+- [Usage Guidelines](#usage-guidelines)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Testing Information](#testing-information)
+- [Contact](#contact)
+
+## Description
+${data.description}
+
+## Installation Instructions
+${data.install}
+
+## Usage Guidelines
+${data.usage}
+
+## Contribution Guidelines
+${data.contribution}
+
+## Testing Information
+${data.test}
+
+## Contact
+[Find me on GitHub](https://github.com/${data.github})
+[Send me an email](mailto:${data.email})
+
 
 `;
 }
